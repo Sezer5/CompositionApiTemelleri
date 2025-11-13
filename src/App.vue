b005e15afdac0cc8e4a216e35eb3dc04c8e77c62
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-13 09:48:27 +0300

    Template Ref Kullanımı

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
     <div>
         <h2>Yaş : {{ yas }}</h2>
-        <h2>Ad Soyad : {{ adSoyad }}</h2>
+        <h2 ref="myRef">Ad Soyad : {{ adSoyad }}</h2>
         <button @click="handleClick">Tıkla</button>
     </div>
 </template>
 
 <script>
-
+import {ref} from 'vue'
 export default {
   name: 'App',
   setup(){
     let adSoyad='Sezer Ünalmış';
     let yas='35';
+    const myRef=ref(null);
     const handleClick = ()=>{
-        console.log('Tıklandı');
+        console.log(myRef,myRef.value);
+        myRef.value.classList.add('can');
+        myRef.value.textContent='Sezer';
+
     }
     return {
-        adSoyad,yas,handleClick
+        adSoyad,yas,handleClick,myRef
     }
   }
 }
