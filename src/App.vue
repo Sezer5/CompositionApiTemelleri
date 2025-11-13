5fa9283aa942dcd355e6c6936cb8ef0df8892903
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-11-13 11:30:25 +0300

    Composition api temelleri bitişi

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
     <div>
         <h2>Yaş : {{ yas }}</h2>
+        <h2>Obje Adı : {{ myObject.name }}</h2>
+        <h2>Obje Adı 2: {{ myObject2.name }}</h2>
         <h2 ref="myRef">Ad Soyad : {{ adSoyad }}</h2>
         <button @click="handleClick">Tıkla</button>
     </div>
 </template>
 
 <script>
-import {ref} from 'vue'
+import {ref,reactive} from 'vue'
 export default {
   name: 'App',
   setup(){
+    const myObject=ref({name:'Sezer',age:'35'});
+    const myObject2=reactive({name:'Sezer',age:'35'});
     const adSoyad=ref('Sezer Ünalmış');
     let yas='35';
     const refyas=ref(35);
@@ -21,10 +25,11 @@ export default {
         // myRef.value.textContent='Sezer';
         adSoyad.value="Deneme";
         console.log(refyas);
+        myObject.value.name="Kevser";
 
     }
     return {
-        adSoyad,yas,handleClick,myRef
+        adSoyad,yas,handleClick,myRef,myObject,myObject2
     }
   }
 }
